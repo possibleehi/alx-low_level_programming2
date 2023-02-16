081b6c0392eca9051b415a2113fdd7a4a6c2606f
possibleehi <possibleehi@gmail.com>
2023-02-16 05:17:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main -prints "Programming is like building a multilingual puzzle, followed by a newline."
- * return 0.
+ * main -prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,5 +11,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }	
